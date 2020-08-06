4872a7fdf070946f154c6669627396476ef8075b
Yusuke Endoh <mame@ruby-lang.org>
2020-08-06 12:11:08 +0900

    Remove NDK_VERSION check

--- setup-android.sh
+++ setup-android.sh
@@ -99,9 +99,9 @@ if [ -e termux-app/app/build/outputs/apk/debug/app-debug.apk ]; then
 else
   log "Checkout and build Termux"
   git clone https://github.com/termux/termux-app.git
-  NDK_VERSION=$(grep ndkVersion termux-app/app/build.gradle | sed "s/ *ndkVersion *'\([0-9.]*\)'/\1/")
-  log "Make sure Android NDK available: '$NDK_VERSION'"
-  sdkmanager --install "ndk;$NDK_VERSION" &>> $SETUP_LOG
+  #NDK_VERSION=$(grep ndkVersion termux-app/gradle.properties | sed "s/ndkVersion=\([0-9.]*\)/\1/")
+  #log "Make sure Android NDK available: '$NDK_VERSION'"
+  #sdkmanager --install "ndk;$NDK_VERSION" &>> $SETUP_LOG
   cd termux-app
   ./gradlew assembleDebug -Pandroid.useAndroidX=true
   cd ..
